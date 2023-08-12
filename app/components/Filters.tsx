1e73e896afdf63f5fd082f957b3eaa066038bf76
Mark Malstrom <mark@malstrom.me>
2023-08-12 11:22:33 -0500

    Add initial filter implementation

---
+++ app/components/Filters.tsx
@@ -0,0 +1,102 @@
+import { ChevronRightIcon } from "@heroicons/react/24/outline"
+import * as Accordion from "@radix-ui/react-accordion"
+import { Link } from "@remix-run/react"
+import { cx } from "class-variance-authority"
+import { Star } from "./Star"
+import { useFilter } from "./context/FilterContext"
+import { useSetScrollPosition } from "./context/ScrollContext"
+
+const staticFilters = [
+    { title: "Tags", options: [] },
+    { title: "Stars", options: [] },
+    { title: "Date", options: [] },
+]
+
+export function Filters() {
+    let [filter] = useFilter()
+
+    return (
+        <Accordion.Root
+            className="mx-20 top-0 sticky h-screen pt-10"
+            collapsible
+            defaultValue={filter.stars !== null ? "Stars" : ""}
+            type="single"
+        >
+            {staticFilters.map(f => (
+                <Accordion.Item
+                    className="border-t-2 border-black px-4 py-6"
+                    key={f.title}
+                    value={f.title}
+                >
+                    {/* as="h3" */}
+                    <Accordion.Header className="-mx-2 -my-3 flow-root">
+                        <Accordion.Trigger className="flex w-full items-center gap-6 px-2 py-3 text-black/50 dark:text-white/50 accordion-trigger">
+                            <span className="flex items-center accordion-icon">
+                                <ChevronRightIcon aria-hidden="true" className="h-5 w-5" />
+                            </span>
+                            <span className="font-medium font-serif-text text-xl text-gray-900 dark:text-gray-50">
+                                {f.title}
+                            </span>
+                        </Accordion.Trigger>
+                    </Accordion.Header>
+                    <Accordion.Content className="accordion-content padding-animation">
+                        <div className="space-y-6">{f.title === "Stars" && <StarOptions />}</div>
+                    </Accordion.Content>
+                </Accordion.Item>
+            ))}
+        </Accordion.Root>
+    )
+}
+
+const stars = [1, 2, 3]
+
+function StarOptions() {
+    let [filter, setFilter] = useFilter()
+    let setScroll = useSetScrollPosition()
+
+    return (
+        <>
+            <Link
+                className={cx(
+                    "font-serif-text text-xl hover:opacity-100 pl-11",
+                    filter.stars === 0 ? "opacity-100" : "opacity-50",
+                )}
+                onClick={event => {
+                    event.preventDefault()
+                    updateURL({ to: event.currentTarget.href })
+                    setScroll(window.scrollY)
+                    setFilter({ stars: 0 })
+                }}
+                to="/?stars=0"
+            >
+                No Stars
+            </Link>
+            {stars.map(num => (
+                <Link
+                    className={cx(
+                        "flex items-center gap-2 hover:opacity-100 pl-11",
+                        filter.stars === num ? "opacity-100" : "opacity-50",
+                    )}
+                    key={`star-wrapper-${num}`}
+                    onClick={event => {
+                        event.preventDefault()
+                        updateURL({ to: event.currentTarget.href })
+                        setScroll(window.scrollY)
+                        setFilter({ stars: num })
+                    }}
+                    to={`/?stars=${num}`}
+                >
+                    {Array(num)
+                        .fill(num)
+                        .map((_, idx) => (
+                            <Star filled={filter.stars === num} key={`star-${idx}`} />
+                        ))}
+                </Link>
+            ))}
+        </>
+    )
+}
+
+function updateURL({ to }: { to: string }) {
+    history.pushState(null, "", to)
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,16 @@
+import { useAutoAnimate } from "@formkit/auto-animate/react"
+import type { LoaderArgs } from "@remix-run/node"
 import { json } from "@remix-run/node"
-import { useLoaderData } from "@remix-run/react"
+import { useLoaderData, useLocation } from "@remix-run/react"
+import { useEffect, useState } from "react"
+import { Filters } from "~/components/Filters"
 import { RecommendationCard } from "~/components/RecommendationCard"
+import { FilterProvider } from "~/components/context/FilterContext"
+import { ScrollProvider } from "~/components/context/ScrollContext"
 import type { Recommendation } from "~/lib/data"
 import notion from "~/lib/notion.server"
 
-export async function loader() {
+export async function loader({ request }: LoaderArgs) {
     let data = await notion.databases.query({
         database_id: process.env.NOTION_DB_ID!,
         sorts: [
@@ -50,13 +56,12 @@ export async function loader() {
             let d = res.properties["Distributor"].select ?? undefined
             if (type === "Album" || type === "Video Game") d = undefined
             let distributor = d ? [{ name: d.name }] : []
-            console.log(distributor)
 
             return {
                 image: res.properties["Image"].url ?? undefined,
                 name,
                 link: res.properties["Info Link"].url,
-                stars: res.properties["Stars"].number,
+                stars: res.properties["Stars"].number ?? 0,
                 description: res.properties["Blurb"].rich_text[0]?.text.content,
                 tags: [
                     { name: type },
@@ -70,27 +75,76 @@ export async function loader() {
         })
         .sort((lhs, rhs) => (rhs.date as any) - (lhs.date as any))
 
-    return json(recs)
+    return json({ recs, url: request.url })
 }
 
 export default function Recommendations() {
-    let recs = useLoaderData<typeof loader>()
+    let { recs: allRecs, url } = useLoaderData<typeof loader>()
+    let queryParams = new URL(url).searchParams
+
+    let [filter, setFilter] = useState<Filter>({
+        stars: queryParams.get("stars") ? parseInt(queryParams.get("stars")!) : null,
+    })
+    let [scroll, setScroll] = useState(0)
+    let [recs, setRecs] = useState(
+        allRecs.filter(rec => {
+            if (filter.stars !== null) {
+                return rec.stars === filter.stars
+            }
+
+            return true
+        }),
+    )
+
+    let location = useLocation()
+
+    let [parentAnimation] = useAutoAnimate()
+
+    // This should only rerun when `recs` updates, not `scroll`
+    // Despite what ESLint says
+    useEffect(() => window.scrollTo(0, scroll), [recs])
+
+    useEffect(() => {
+        let searchParams = new URLSearchParams(location.search)
+        setFilter({
+            stars: searchParams.get("stars") ? parseInt(searchParams.get("stars")!) : null,
+        })
+    }, [location])
+
+    useEffect(() => {
+        console.log(location.search)
+        setRecs(
+            allRecs.filter(rec => {
+                if (filter.stars !== null) {
+                    return rec.stars === filter.stars
+                }
+
+                return true
+            }),
+        )
+    }, [location, filter, allRecs])
 
     return (
         <div className="noise-container p-6">
             <div className="noise" />
             <div style={{ zIndex: "-30" }} />
-            <h1 className="font-serif-display font-bold text-5xl sm:text-6xl mb-6">
-                Recommendations
-            </h1>
+            <h1 className="font-serif-display font-bold text-5xl sm:text-6xl">Recommendations</h1>
             <div className="sm:grid-cols-[3fr_2fr] sm:grid block">
-                <div className="flex flex-col items-start gap-7 pb-6">
+                <div className="flex flex-col items-start gap-7 py-10" ref={parentAnimation}>
                     {recs.map(rec => (
                         <RecommendationCard key={rec.name} recommendation={rec} />
                     ))}
                 </div>
-                {/* TODO: Search & filters */}
+                <FilterProvider value={[filter, setFilter]}>
+                    <ScrollProvider value={setScroll}>
+                        <Filters />
+                    </ScrollProvider>
+                </FilterProvider>
             </div>
         </div>
     )
+}
+
+export interface Filter {
+    stars: number | null
 }
