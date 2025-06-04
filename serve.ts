7cc8fb242de59d32e4ea7ae975d8901c488ce930
Mark Malstrom <mark@malstrom.me>
2025-06-03 22:05:14 -0500

    Fix serve.ts

--- serve.ts
+++ serve.ts
@@ -1,4 +1,5 @@
 import { serveDir, serveFile } from "@std/http/file-server";
+import { join } from "@std/path";
 import { createRequestHandler } from "react-router";
 
 const handler = createRequestHandler(
@@ -7,24 +8,40 @@ const handler = createRequestHandler(
     "production",
 );
 
-const port = Number.parseInt(Deno.env.get("PORT") || "3000");
+const port = Number.parseInt(Deno.env.get("PORT") || "8080");
 
 console.log("Starting production server...");
 Deno.serve({ port }, async (request: Request): Promise<Response> => {
     const pathname = new URL(request.url).pathname;
 
-    if (pathname === "/favicon.ico") {
-        return serveFile(request, "build/client/favicon.ico");
-    }
+    try {
+        const filePath = join("./build/client", pathname);
+        const fileInfo = await Deno.stat(filePath);
 
-    if (pathname.startsWith("/assets/")) {
-        return serveDir(request, {
-            fsRoot: "build/client/assets",
-            urlRoot: "assets",
-            headers: [
-                "Cache-Control: public, max-age=31536000, immutable",
-            ],
-        });
+        if (fileInfo.isDirectory) {
+            throw new Deno.errors.NotFound();
+        }
+
+        // The request is for a static file that exists
+
+        const response = await serveFile(request, filePath, { fileInfo });
+        response.headers.set("cache-control", "public, max-age=600");
+
+        if (pathname.startsWith("/assets/")) {
+            return serveDir(request, {
+                fsRoot: "build/client/assets",
+                urlRoot: "assets",
+                headers: [
+                    "Cache-Control: public, max-age=31536000, immutable",
+                ],
+            });
+        }
+
+        return response;
+    } catch (error) {
+        if (!(error instanceof Deno.errors.NotFound)) {
+            throw error;
+        }
     }
 
     return await handler(request);
